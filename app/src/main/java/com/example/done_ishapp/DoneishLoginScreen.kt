fa11ed473ba42c08096583013ac45f5c372509fd
SarasotaSmile20 <sarasotasmile20@outlook.com>
2025-05-21 12:11:53 -0400

    Done-ish app prototype for mental momentum

---
+++ app/src/main/java/com/example/done_ishapp/DoneishLoginScreen.kt
@@ -0,0 +1,2 @@
+package com.example.done_ishapp
+
---
+++ app/src/main/java/com/example/done_ishapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.done_ishapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.done_ishapp.ui.theme.DoneishAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DoneishAppTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "welcome") {
+                    composable("welcome") {
+                        DoneishWelcomeScreen(navController)
+                    }
+                    composable("login") {
+                        DoneishLoginScreen(navController)
+                    }
+                }
+            }
+        }
+    }
+}
